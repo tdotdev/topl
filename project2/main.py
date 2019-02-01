4799e917fd0f0fb8532bbd2e871628152cb6626c
tcs <tcs49@zips.uakron.edu>
2019-01-31 20:56:57 -0500

    init scripts

---
+++ project2/main.py
@@ -0,0 +1,16 @@
+
+
+class expr():
+    def __init__(self):
+        pass
+
+class binary_expr(expr):
+    def __init__(self):
+        pass
+
+class not_expr(expr):
+    def __init__(self):
+        pass
+
+if __name__ == '__main__':
+    print('Running, boss!')
